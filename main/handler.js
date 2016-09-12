60249d487ba123bea04b7376fc43d8b87ed08e99
Orion Free <ofree@upperquadrant.com>
2016-09-11 22:59:34 -0400

    Rough beginning

---
+++ main/handler.js
@@ -0,0 +1,29 @@
+'use strict';
+var blogmain = require('../shared/main.node');
+
+// Your first function handler
+module.exports.main = (event, context, cb) => {
+
+  var posts = [
+    {
+      id: '123',
+      title: 'New Post',
+      date: new Date(),
+      snippet: 'Meadfee...',
+      url: 'new_post'
+    },
+    {
+      id: '124',
+      title: 'Presentation',
+      date: new Date(),
+      snippet: 'Back in March, I [gave a presentation](/refi/) at Redisconf.<br /><br />Here is the video! In hindsight...',
+      url: 'new_post'
+    }
+      
+  ];
+
+  var html = blogmain.build(posts);
+  context.succeed(html);
+};
+
+// You can add more handlers here, and reference them in serverless.yml
---
+++ page/handler.js
@@ -0,0 +1,17 @@
+'use strict';
+let blogpage = require('../shared/pageBuild.node')
+
+// Your first function handler
+module.exports.main = (event, context, cb) => {
+  let post = {
+    id: '123',
+    content: 'Back in March, I [gave a presentation](/refi/) at Redisconf. \n\n Here is the video! In hindsight, I would have approached it somewhat differently, but it was alright for a start. \n\n <video xmlns="https://www.w3.org/1999/xhtml" width="461" height="259" id="bcVideo" src="https://uds.ak.o.brightcove.com/2660431281001/2660431281001&#95;4137035063001&#95;Redis-Orion-Free.mp4" controls="controls"></video> \n\n You can get the accompanying slides [here](http://file.notsafeforproduction.com/servertoservercomm.pptx)\n\n',
+    url: 'presentation',
+    title: 'Presentation',
+    tags: ['presentation', 'redis']
+  };
+  var html = blogpage.build(post.content);
+  context.succeed(html);
+};
+
+// You can add more handlers here, and reference them in serverless.yml
