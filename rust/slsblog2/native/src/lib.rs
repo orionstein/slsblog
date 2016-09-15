31dcda502ea8792ec0e1e292442df83a030861cc
Orion Free <orionstein@gmail.com>
2016-09-15 02:17:31 +0000

    updated

--- rust/slsblog2/native/src/lib.rs
+++ rust/slsblog2/native/src/lib.rs
@@ -4,13 +4,13 @@ extern crate handlebars;
 extern crate rustc_serialize;
 extern crate glob;
 use neon::vm::{Call, JsResult, Module};
-use neon::js::JsString;
+use neon::js::{JsString, JsObject, JsArray, JsValue, Object, Value};
+use neon::mem::{Handle, Managed};
 use std::path::Path;
 use handlebars::Handlebars;
 use std::collections::BTreeMap;
 use rustc_serialize::json::{Json, ToJson};
 use glob::glob;
-
 
 fn build(call: Call) -> JsResult<JsString> {
     let scope = call.scope;
@@ -27,8 +27,48 @@ fn build(call: Call) -> JsResult<JsString> {
 	    }
     }
     
+
+    let mut blogData: BTreeMap<String, Json> = BTreeMap::new();
+    let blog: Handle<JsObject> = try!(try!(call.arguments.require(scope, 0)).check::<JsObject>());
+    let blogItems = try!(blog.get_own_property_names(scope));
+    println!("bi {}", blogItems.len());
+    let blogItemIter = (blogItems).to_vec(scope);
+    for prop in &blogItemIter {
+      for entry in &*prop {
+        let nameString = try!(JsValue::to_string((**entry), scope));
+        let nameExtract = (*nameString).value();
+        let item = try!(blog.get(scope, nameString));
+        let bItem = try!(JsValue::to_string((*item), scope));
+        let bItemExtract = (*bItem).value();
+        println!("{:?}", nameExtract);
+        println!("{:?}", bItemExtract);
+        blogData.insert(nameExtract.to_string(), bItemExtract.to_json());
+      }
+    }
+
+    let mut postArrData = vec![];
+    let posts: Handle<JsArray> = try!(try!(call.arguments.require(scope, 1)).check::<JsArray>());
+    println!("pi {}", posts.len());
+    let postIter = try!((posts).to_vec(scope));
+    for post in &postIter {
+      let mut postData: BTreeMap<String, Json> = BTreeMap::new();
+      let postObj: Handle<JsObject> = try!(post.check::<JsObject>());
+      let id = try!((postObj).get(scope, "id"));
+      let ids = try!(JsValue::to_string((*id), scope));
+      let ide = (*ids).value();
+      let title = try!((postObj).get(scope, "title"));
+      let titles = try!(JsValue::to_string((*title), scope));
+      let titlee = (*titles).value();
+      println!("{:?}", ide);
+      println!("{:?}", titlee);
+      postData.insert("id".to_string(), ide.to_json());
+      postData.insert("title".to_string(), titlee.to_json());
+      postArrData.push(postData.to_json());
+    }
+
     let mut data: BTreeMap<String, Json> = BTreeMap::new();
-    data.insert("title".to_string(), "base0".to_json());
+    data.insert("blog".to_string(), blogData.to_json());
+    data.insert("posts".to_string(), postArrData.to_json());
     let dataJson = data.to_json();
 
     let pageString = handlebars.render("index", &dataJson).ok().unwrap();
